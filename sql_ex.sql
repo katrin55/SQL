bdf7769e2776a4112866761512ccaf3fea005642
katrin55 <98700115+katrin55@users.noreply.github.com>
2023-02-15 15:12:46 +0300

    'задания с сайта sql-ex.ru'

--- sql_ex.sql
+++ sql_ex.sql
@@ -2,7 +2,7 @@
 -- Product(maker, model, type)
 -- PC(code, model, speed, ram, hd, cd, price)
 -- Laptop(code, model, speed, ram, hd, price, screen)
--- Printer(code, model, color, type, price)
+-- Printer(code, model, color, type, price).
 -- Таблица Product представляет производителя (maker), номер модели (model) и тип ('PC' - ПК, 'Laptop' - ПК-блокнот или 'Printer' - принтер).
 -- Предполагается, что номера моделей в таблице Product уникальны для всех производителей и типов продуктов. 
 -- В таблице PC для каждого ПК, однозначно определяемого уникальным кодом – code, указаны модель – model (внешний ключ к таблице Product), скорость - speed (процессора в мегагерцах), объем памяти - ram (в мегабайтах), размер диска - hd (в гигабайтах), скорость считывающего устройства - cd (например, '4x') и цена - price (в долларах). 
